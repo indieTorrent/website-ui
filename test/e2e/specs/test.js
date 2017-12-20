ce86412779bc1db07a1ee8d16e33621ae48f0262
poppabear8883 <servnx@gmail.com>
2017-12-20 11:14:41 -0500

    tests: make tests green

--- test/e2e/specs/test.js
+++ test/e2e/specs/test.js
@@ -12,7 +12,7 @@ module.exports = {
       .url(devServer)
       .waitForElementVisible('#app', 5000)
       .assert.elementPresent('.hello')
-      .assert.containsText('h1', 'Welcome to indieTorrent, LLC')
+      .assert.containsText('h1', 'Welcome to indieTorrent.org, LLC')
       .end();
   },
 };
